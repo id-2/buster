51e3c0cc2ec7d6943a84ebb697df8a3adccfbd03
dal <dallin@buster.so>
2025-01-09 09:23:46 -0700

    Add new endpoint for retrieving dataset data samples
    - Introduced a new route to the datasets API for fetching data samples associated with a specific dataset.
    - Added the `get_dataset_data_sample` module to handle the logic for retrieving dataset data samples.
    - Updated the router configuration to include the new endpoint, enhancing the API's functionality for dataset management.
    
    These changes improve the API's capabilities by allowing users to access sample data for datasets, facilitating better data exploration and analysis.

--- api/src/routes/rest/routes/datasets/mod.rs
+++ api/src/routes/rest/routes/datasets/mod.rs
@@ -1,5 +1,6 @@
 mod assets;
 mod get_dataset;
+mod get_dataset_data_sample;
 mod list_datasets;
 mod post_datasets;
 
@@ -13,5 +14,9 @@ pub fn router() -> Router {
         .route("/", get(list_datasets::list_datasets))
         .route("/", post(post_datasets::post_datasets))
         .route("/:dataset_id", get(get_dataset::get_dataset))
+        .route(
+            "/:dataset_id/data/sample",
+            get(get_dataset_data_sample::get_dataset_data_sample),
+        )
         .nest("/:dataset_id", assets::router())
 }
